45d18060cef9328f6af45900edb55fafcc93d837
R Aravind <aravind.ar786@gmail.com>
2019-07-11 22:40:35 +0530

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,33 @@
 let result = document.getElementById("search-results");
 let featured = document.getElementById("featured");
 let SearchInput = document.getElementById("search-input")
+let hos = document.getElementById("f_hos");
+let docs = document.getElementById("f_docs")
 
 //Search(); //FOR TESTING PURPOSES
 
-let hospital = {
-    name: "lol",
-    rating: 3,
-    price: 2000
-}
 
 function Search() {
     let value = SearchInput.value;
 
     // DESIGN AND ANIMATION CHANGES
     result.style.visibility = "visible";
-    result.style.height = "30vh";
-    result.style.transition = "height 0.9s";
-    featured.style.height = "210vh";
 
+    // PC 
+    if (window.matchMedia("(min-width:1024px)").matches) {
+        result.style.height = "30vh";
+        result.style.transition = "height 0.9s";
+        featured.style.height = "240vh";
+    }
 
-
+    // RESPONSIVE
+    if (window.matchMedia("only screen and (max-width:1024px)").matches) {
+        hos.innerHTML = "";
+        docs.innerHTML = "";
+        featured.style.height = "190vh";
+        result.style.height = "180vh";
+        result.style.transition = "height 0.9s";
+    }
 
     console.log(value);
 }
