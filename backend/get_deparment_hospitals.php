b83138ef18120e02aee2b14dbee8edf0cc8fa9c9
jack <imakhilravindran@gmail.com>
2019-07-12 00:07:00 +0530

    Add api link for get hospital names by deparment
    *Also change name with prefix get_
    *Added a api reference text

---
+++ backend/get_deparment_hospitals.php
@@ -0,0 +1,27 @@
+
+<?php 
+    require_once "database.php";
+
+    $department = $_GET['department'];
+
+    $hospitals = array();
+
+    if(isset($_GET['department'])){
+        $data = getRawData("SELECT * FROM `hospital`");
+
+        // echo count($data)s;
+
+        for($i=0;$i<count($data);$i++){
+            $sectors = unserialize($data[$i]['sectors']);
+            if(array_search($department, $sectors)){
+                $hospitals[] = $data[$i]['id'];
+            }
+
+        }
+
+        // print_r(unserialize($data[0]['sectors']));
+    }
+
+// ?department=department
+
+?>
