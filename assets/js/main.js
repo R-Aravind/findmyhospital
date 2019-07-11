6b497af89f15b1754327961e9a489974107d041a
jack <imakhilravindran@gmail.com>
2019-07-12 00:30:05 +0530

    Some minor change in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
-let result = document.getElementById("search-results");
-let featured = document.getElementById("featured");
-let SearchInput = document.getElementById("search-input")
-let hos = document.getElementById("f_hos");
-let docs = document.getElementById("f_docs")
+const result = document.getElementById("search-results");
+const featured = document.getElementById("featured");
+const SearchInput = document.getElementById("search-input")
+const hos = document.getElementById("f_hos");
+const docs = document.getElementById("f_docs")
 
 //Search(); //FOR TESTING PURPOSES
 
