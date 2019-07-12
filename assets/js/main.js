aaecab831c3f54f91b185bb517c484b96c8cb74f
jack <imakhilravindran@gmail.com>
2019-07-12 11:49:23 +0530

    Added featured hospital, doctors and search results from database

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,21 @@ const docs = document.getElementById("f_docs")
 
 // const db = new HosAPI(); //create new 
 
+window.onload = function () { 
+    get_featured_doctors('.f_docs');
+    get_featured_hospitals('.f_hos');
+    
+ }
+
+
 function Search() {
 
 
     let value = SearchInput.value;
 
     // console.log(db.fetch_data('hello'));
-
-    get_featured_doctors('.container');
+    get_hospital_location('#search-results', value);
+    
 
     // let data = db.get_hospital_name('Hospital 1');
     // console.log(data);
